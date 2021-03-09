80862b28e693d0c906d6d0716c3596aa0e93c9c7
avatar9966 <av4t@hotmail.com>
2021-03-09 20:58:47 -0300

    refactor: change express to use json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express, { response } from 'express'
+import 'reflect-metadata'
+import express, { request, response } from 'express'
+import { router } from './routes'
+import './database'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.send({ message: 'Hello Word - NLW04' })
-})
-
-app.post('/', (request, response) => {
-  return response.send({ message: 'Os dados foram salvos com sucesso!' })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log('Server is running!'))
