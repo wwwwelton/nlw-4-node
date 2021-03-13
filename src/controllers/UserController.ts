7d3e662ca2b2507cad5416af193bfdceeeeee9e5
avatar9966 <av4t@hotmail.com>
2021-03-12 23:30:59 -0300

    refactor: validate create user fields

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import { UsersRepository } from '../repositories/UsersRepository'
+import * as yup from 'yup'
+import { AppError } from '../errors/AppError'
 
 class UserController {
 
   async create (request: Request, response: Response) {
     const { name, email } = request.body
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    })
+
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+    } catch (error) {
+      throw new AppError(error)
+    }
 
     const usersRepository = getCustomRepository(UsersRepository)
 
@@ -14,9 +27,7 @@ class UserController {
     })
 
     if (userAlreadyExists) {
-      return response.status(400).json({
-        error: 'User already exists!'
-      })
+      throw new AppError('User already exists!')
     }
 
     const user = usersRepository.create({
